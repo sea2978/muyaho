a3c14c2371b767b1cccb44e1c307fad8f716fef8
G-02\G <a01057510981@gmail.com>
2021-03-28 18:04:27 +0900

    공격주고받기2

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -40,21 +40,40 @@ namespace test
 
     class Character
     {
+
         public float maxhp;
         public float hp;
-        public float power;
+        public int power;
         public string name;
+        public float damage;
 
         public void Attack(Character target)
         {
-            Console.WriteLine($"{this.name}가 {target.name}를 {this.power}의 데미지로 공격");
-            target.hp -= this.power;
+            Random ran = new Random();
+            damage = ran.Next(0, this.power + 1);
+
+            Console.WriteLine($"{this.name}가 {target.name}를 {this.damage}의 데미지로 공격");
+
+            target.hp -= this.damage;
             Hit(this, target);
         }
         public void Hit(Character hitter,Character target)
         {
-            Console.WriteLine($"{target.name}가 {hitter.name}로부터 {hitter.power}의 데미지를 입음 " +
-                $"{target.name}의 남은 hp : {target.hp}");
+            if(hitter.damage == 0)
+            {
+                Console.WriteLine($"{target.name}가 {hitter.name}의 공격을 피함 " +
+                    $"{target.name}의 남은 hp : {target.hp}");
+            }
+            if (hitter.damage == hitter.power)
+            {
+                Console.WriteLine($"{hitter.name}가 {target.name}의 급소를 공격함 " +
+                    $"{target.name}의 남은 hp : {target.hp}");
+            }
+            else
+            {
+                Console.WriteLine($"{target.name}가 {hitter.name}로부터 {hitter.damage}의 데미지를 입음 " +
+                    $"{target.name}의 남은 hp : {target.hp}");
+            }
         }
     }
 
