d96e0dbe6980291f3d6cb57995926ff302a5ac16
G-02\G <a01057510981@gmail.com>
2021-03-28 17:11:28 +0900

    no message

---
+++ interfacePractice/interfacePractice/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfacePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Person me = new Person();
+            Calculator calc = new Calculator();
+
+            DoSomething(me);
+            DoSomething(calc);
+        }
+
+        static void DoSomething(iSum target)
+        {
+            int result = target.Sum(100, 100);
+            Console.WriteLine(result);
+        }
+    }
+
+    class Person : iSum
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b + 1;
+        }
+    }
+
+    class Calculator : iSum
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    interface iSum
+    {
+        int Sum(int a, int b);
+    }
+}
